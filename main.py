dc8d814bef36575b203f137762719347c66151e3
snc-jun <junichiro@snc-jp.com>
2025-05-20 00:59:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template, redirect, url_for
+import os
+from crawler import run_crawler
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    image_dir = "static/captures"
+    images = sorted(os.listdir(image_dir)) if os.path.exists(image_dir) else []
+    return render_template("index.html", images=images)
+
+@app.route("/run")
+def run():
+    run_crawler()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run()
